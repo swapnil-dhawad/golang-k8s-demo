747657ae258c4f749b55ea2059f2ab902e250cc5
Swapnil Dhawad <swapdhawad@gmail.com>
2024-07-30 18:30:03 +0100

    fix: terraform porvider fix

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,4 @@
-
+/*
 terraform {
   required_version = ">= 0.12"
   required_providers {
@@ -26,6 +26,25 @@ terraform {
       source  = "hashicorp/cloudinit"
       version = "~> 2.2.0"
     }
+  }
+}
+*/terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+
   }
  backend "remote" {
 		hostname = "app.terraform.io"
