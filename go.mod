40f16975645ac69af51cb4382c2e2c5e30da8255
Swapnil Dhawad <swapdhawad@gmail.com>
2024-08-01 12:09:44 +0100

    fix: telemetry testing

--- go.mod
+++ go.mod
@@ -1,2 +1,4 @@
 module github.com/swapnil-dhawad/golang-k8s-demo
 go 1.22.5
+go get github.com/prometheus/client_golang/prometheus
+go get github.com/prometheus/client_golang/prometheus/promhttp
--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var (
+    httpRequests = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Name: "http_requests_total",
+            Help: "Total number of HTTP requests",
+        },
+        []string{"method", "handler"},
+    )
+)
 
-func welcomePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/welcome.html")
+func init() {
+    prometheus.MustRegister(httpRequests)
 }
 
-
+func welcomePage(w http.ResponseWriter, r *http.Request) {
+    httpRequests.WithLabelValues(r.Method, "welcomePage").Inc()
+    // Render the course html page
+    http.ServeFile(w, r, "static/welcome.html")
+}
 
 func main() {
-
-	http.HandleFunc("/", welcomePage)
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    http.HandleFunc("/", welcomePage)
+    http.Handle("/metrics", promhttp.Handler()) // Expose metrics endpoint
+    err := http.ListenAndServe("0.0.0.0:8080", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
