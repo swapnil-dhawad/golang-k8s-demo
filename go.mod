9a855ae8b217263fe74caae0a68f6ac04cfa9d44
Swapnil Dhawad <swapdhawad@gmail.com>
2024-08-01 12:28:57 +0100

    fix: dockerfile issue

--- go.mod
+++ go.mod
@@ -1,4 +1,2 @@
 module github.com/swapnil-dhawad/golang-k8s-demo
 go 1.22.5
-go get github.com/prometheus/client_golang/prometheus
-go get github.com/prometheus/client_golang/prometheus/promhttp
--- main.go
+++ main.go
@@ -1,37 +1,26 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
 )
 
-var (
-    httpRequests = prometheus.NewCounterVec(
-        prometheus.CounterOpts{
-            Name: "http_requests_total",
-            Help: "Total number of HTTP requests",
-        },
-        []string{"method", "handler"},
-    )
-)
-
-func init() {
-    prometheus.MustRegister(httpRequests)
-}
 
 func welcomePage(w http.ResponseWriter, r *http.Request) {
-    httpRequests.WithLabelValues(r.Method, "welcomePage").Inc()
-    // Render the course html page
-    http.ServeFile(w, r, "static/welcome.html")
+	// Render the course html page
+	http.ServeFile(w, r, "static/welcome.html")
 }
 
+
+
 func main() {
-    http.HandleFunc("/", welcomePage)
-    http.Handle("/metrics", promhttp.Handler()) // Expose metrics endpoint
-    err := http.ListenAndServe("0.0.0.0:8080", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+
+	http.HandleFunc("/", welcomePage)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
+
+
